0ecadff66da163d0938fa905f0b4ec2bfd2f3e61
Gina <ginamantonini@gmail.com>
2018-01-14 13:37:40 -0600

    added employee list and method to add or remove an employee

---
+++ MetropolitanPropertyMgmt/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MetropolitanPropertyMgmt
+{
+    public class Employee
+    {
+        protected string _firstName;
+        protected string _lastName;
+        protected string _jobTitle;
+        protected string _startDate;
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+           
+        public string JobTitle
+        {
+            get
+            {
+                return _jobTitle;
+            }
+            set
+            {
+                _jobTitle = value;
+            }
+        }
+        public string StartDate
+        {
+            get
+            {
+                return _startDate;
+            }
+            set
+            {
+                _startDate = value;
+            }
+        }
+    }
+}
--- MetropolitanPropertyMgmt/Program.cs
+++ MetropolitanPropertyMgmt/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MetropolitanPropertyMgmt
 {
@@ -22,17 +19,28 @@ namespace MetropolitanPropertyMgmt
         public DateTime CreatedOn { get; }
 
         // Create a property for holding a list of current employees
+        public void EmployeeList()
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee(){ FirstName = "Gina", LastName = "Antonini", JobTitle = "Director of Operations", StartDate = "11/1/16" },
+                new Employee(){ FirstName = "Haley", LastName = "Adamas", JobTitle = "Assistant Director of Operations", StartDate = "1/2/17" },
+                new Employee(){ FirstName = "Tyler", LastName = "Hall", JobTitle = "Property Manager", StartDate = "1/5/14" },
+            };
 
-        // Create a method that allows external code to add an employee
+            // Create a method that allows external code to add an employee
+            employees.Add(new Employee() { FirstName = "Jacie", LastName = "Rush", JobTitle = "Administrative Assistant", StartDate = "2/1/18" });
 
-        // Create a method that allows external code to remove an employee
+            // Create a method that allows external code to remove an employee
+            employees.Remove(new Employee() { FirstName = "Jacie", LastName = "Rush", JobTitle = "Administrative Assistant", StartDate = "2/1/18" });
+        }
 
-        /*
-            Create a constructor method that accepts two arguments:
-                1. The name of the company
-                2. The date it was created
+    /*
+        Create a constructor method that accepts two arguments:
+            1. The name of the company
+            2. The date it was created
 
-            The constructor will set the value of the public properties
-        */
-    }
+        The constructor will set the value of the public properties
+    */
 }
+    }
